6df08d7f462663f89a7d64bfb26587cc81f347a1
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-04-25 19:50:41 +0530

    src to doc

---
+++ docs/index.js
@@ -0,0 +1,20 @@
+
+
+let verdict_div = document.getElementById('verdict')
+async function login() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+    if (username !== '' && password !== '') {
+        console.log(JSON.stringify({ username: username, password: password }))
+        let verdict = await axios.post('http://localhost:3000/users/login', {
+            username: username,
+            password: password
+        })
+        console.log("Verdict :", verdict.data)
+        verdict_div.innerHTML = `<h3>${verdict.data}</h3>`
+    }
+
+    document.getElementById('username').value = ``
+    document.getElementById('password').value = ``
+
+}
